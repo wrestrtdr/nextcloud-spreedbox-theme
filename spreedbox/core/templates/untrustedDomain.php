3487699ab9a9beff77ff802bfaa9da47e1769573
Joachim Bauch <bauch@struktur.de>
2016-02-15 17:50:02 +0100

    Example configuration is in /usr/share/doc

--- spreedbox/core/templates/untrustedDomain.php
+++ spreedbox/core/templates/untrustedDomain.php
@@ -5,7 +5,7 @@
 		<?php p($l->t('You are accessing the server from the untrusted domain "%s".', $_['domain'])); ?><br>
 
 		<p class='hint'>
-			<?php p($l->t('Please contact your administrator. If you are an administrator of this instance, add the domain "%s" to "/etc/owncloud/additional_trusted_domains.conf" and run "spreedbox-update-trusted-domains" to activate the new settings. An example configuration is provided in "/etc/owncloud/additional_trusted_domains.conf.sample".', $_['domain'])); ?>
+			<?php p($l->t('Please contact your administrator. If you are an administrator of this instance, add the domain "%s" to "/etc/owncloud/additional_trusted_domains.conf" and run "spreedbox-update-trusted-domains" to activate the new settings. An example configuration is provided in "/usr/share/doc/spreedbox-owncloud/additional_trusted_domains.conf.example".', $_['domain'])); ?>
 			<br>
 		</p>
 	</li>
