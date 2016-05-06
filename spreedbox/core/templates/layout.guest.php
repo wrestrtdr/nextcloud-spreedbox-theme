34f0c5c699c1d0aa41d73526a15eca4dfeac34b9
Leon Klingele <leon@struktur.de>
2016-05-06 17:11:32 +0200

    Add mask-icon

--- spreedbox/core/templates/layout.guest.php
+++ spreedbox/core/templates/layout.guest.php
@@ -14,6 +14,7 @@
 		<meta name="theme-color" content="<?php p($theme->getMailHeaderColor()); ?>">
 		<link rel="icon" href="<?php print_unescaped(image_path('', 'favicon.ico')); /* IE11+ supports png */ ?>">
 		<link rel="apple-touch-icon-precomposed" href="<?php print_unescaped(image_path('', 'favicon-touch.png')); ?>">
+		<link rel="mask-icon" sizes="any" href="<?php print_unescaped(image_path('', 'favicon-mask.svg')); ?>" color="#67c7e3">
 		<?php foreach($_['cssfiles'] as $cssfile): ?>
 			<link rel="stylesheet" href="<?php print_unescaped($cssfile); ?>">
 		<?php endforeach; ?>
